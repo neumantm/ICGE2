7c57de30d673038ec462558c2ed98e51bc87d743
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2020-07-25 16:23:00 +0200

    Test 1

--- scriptsAndTools/fixPomFormat.sh
+++ scriptsAndTools/fixPomFormat.sh
@@ -15,6 +15,8 @@ if ! which xmlstarlet >/dev/null 2>&1 ;then
   fail "Need program xmlstarlet" 3
 fi
 
+exit 1
+
 operation='mv "{}.2" "{}"'
 
 bash "$dir/internalOrCiOnly/formatPomsAndRunOperation.sh" "--i-know-what-i-am-doing" "$operation"
